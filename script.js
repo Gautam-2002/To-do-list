6041eacbc7dbadb4c1f9af3625e8670b0445e9b1
Gautam-2002 <gautam2sethi@gmail.com>
2022-09-25 18:33:58 +0530

    buttons added

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ const ul = document.querySelector('#ul')
 
 btn.addEventListener("click", ()=>{
     const li = document.createElement("li");
+
+    const mydiv = document.createElement("div");
+
+    const txt = document.createTextNode(inp.value)
+    mydiv.appendChild(txt);
+
     const del = document.createElement("button");
     del.innerText = "X";
-    li.appendChild(del);
-    li.innerText = inp.value;
+    mydiv.insertBefore(del, mydiv.lastChild);
+
+    const up = document.createElement("button");
+    up.innerText = "↑";
+    mydiv.insertBefore(up, mydiv.lastChild);
+
+    const down = document.createElement("button");
+    down.innerText = "↓";
+    mydiv.insertBefore(down,mydiv.lastChild);
+    
+    li.appendChild(mydiv);
     ul.appendChild(li);
     inp.value = "";
 });
