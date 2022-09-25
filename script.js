9747e7e66b6662236547ac049f568f5d498dade8
Gautam-2002 <gautam2sethi@gmail.com>
2022-09-25 18:54:36 +0530

    style classes added

--- script.js
+++ script.js
@@ -6,22 +6,26 @@ btn.addEventListener("click", ()=>{
     const li = document.createElement("li");
 
     const mydiv = document.createElement("div");
+    mydiv.classList.add("mydiv");
 
     const txt = document.createTextNode(inp.value)
     mydiv.appendChild(txt);
 
     const del = document.createElement("button");
+    del.classList.add("del");
     del.innerText = "X";
     mydiv.insertBefore(del, mydiv.lastChild);
 
     const up = document.createElement("button");
+    up.classList.add("up");
     up.innerText = "↑";
     mydiv.insertBefore(up, mydiv.lastChild);
 
     const down = document.createElement("button");
+    down.classList.add("down");
     down.innerText = "↓";
     mydiv.insertBefore(down,mydiv.lastChild);
-    
+
     li.appendChild(mydiv);
     ul.appendChild(li);
     inp.value = "";
